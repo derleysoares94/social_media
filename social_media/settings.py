b8a4f13a570da9adc76d259c790aa180b6529f78
Wanderley Soares Feitosa <wanderleysoares1994@gmail.com>
2025-02-17 22:09:09 +0000

    Recovery email

--- social_media/settings.py
+++ social_media/settings.py
@@ -136,3 +136,10 @@ LOGIN_REDIRECT_URL = 'communities-home'
 LOGIN_URL = 'login'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'] # Login email address, Stored as an environment varaible
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD'] # App Password set from gmail, Stored as an environment varaible
